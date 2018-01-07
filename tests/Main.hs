2487e043dd0515bae9213381ca068d38d3287e0d
Sebastian Graf <sgraf1337@gmail.com>
2018-01-07 11:51:11 +0100

    Fixed stability of tests by deactivating parallelism

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,15 +1,20 @@
 import qualified Critical
 import qualified StrAnal
+import           System.Environment
 import           Test.Tasty
 import qualified Trivial
 
 main :: IO ()
-main = defaultMain $ testGroup "All tests"
-  [ testGroup "Unit test"
-      [ testGroup "Trivial" Trivial.tests
-      , testGroup "Critical cases" Critical.tests
-      ]
-  , testGroup "Analyses"
-      [ testGroup "Strictness" StrAnal.tests
-      ]
-  ]
+main = do
+  -- Parallel tests get us in trouble because of
+  -- locks on the package db in GHC 8.2.2
+  setEnv "TASTY_NUM_THREADS" "1"
+  defaultMain $ testGroup "All tests"
+    [ testGroup "Unit test"
+        [ testGroup "Trivial" Trivial.tests
+        , testGroup "Critical cases" Critical.tests
+        ]
+    , testGroup "Analyses"
+        [ testGroup "Strictness" StrAnal.tests
+        ]
+    ]
