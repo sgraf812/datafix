29f6799b77238b428310f210bec35833b5cb52cd
Sebastian Graf <sgraf1337@gmail.com>
2020-01-11 18:13:48 +0100

    Fix cabal check output

--- examples/SetRecurrences/FirstFollow.hs
+++ examples/SetRecurrences/FirstFollow.hs
@@ -20,7 +20,6 @@ module SetRecurrences.FirstFollow
   , llsll
   , emptyL
   , leftrec
-  , main
   ) where
 
 import           Prelude hiding (words)
@@ -203,19 +202,3 @@ leftrec = augmentGrammar 'O' $ mkGrammar
   , ('A', "c")
   ]
   'S'
-
-main :: IO ()
-main = do
-  (k:_) <- map read <$> getArgs
-  let uncurry3 f (x,y,z) = f x y z
-  let analyse name gr (s :: Char) = do
-        printf "%s:\n" name
-        printf "  first_%d(%s): %s\n" k (show s) (show $ first k gr [NT s])
-        printf "  follow_%d(%s): %s\n" k (show s) (show $ follow k gr s)
-        putStrLn ""
-  mapM_ (uncurry3 analyse) $
-    [ ("Dyck", dyck, 'S')
-    , ("LL(1), not SLL(k)", llsll, 'A')
-    , ("empty", emptyL, 'A')
-    , ("left recursive", leftrec, 'S')
-    ]
